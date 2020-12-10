2c8ddabf4d74f1a3c9ecd1d90b83ce8fa67345df
IchikawaKoki <k.kou.1kw5.13@icloud.com>
2020-12-10 14:39:21 +0000

    [Finish]2-7

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd382dce66511605183a902e8b1890b03ded530d7d0e9a8d4e3117255d075034a0054eca8dd0a1ce19c94f5b9990ba4655b9829b39a90437d42cdceec2d9ddfda'
